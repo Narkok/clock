2112480e37eb29764d9215d1da098a79af70eecd
Narkok <nar-ste@yandex.ru>
2019-06-18 00:31:00 +0300

    first commit

---
+++ clock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clock
+//
+//  Created by Narek Stepanyan on 18/06/2019.
+//  Copyright © 2019 NRKK dev.studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
